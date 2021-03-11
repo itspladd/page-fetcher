1075e6948d75ea8ce56b68cd4661bd80d580c608
Paul Ladd <vlendle@gmail.com>
2021-03-11 14:59:07 +0000

    Added internal checking for file existence before writing, no user input yet

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const fs = require('fs');
+const rl = require('readline');
 
 const url = process.argv[2];
 const path = process.argv[3];
@@ -11,7 +12,9 @@ request(url, (error, response, body) => {
   if (error) {
     reportResults(path, url, error);
   } else {
-    fs.writeFile(path, body, err => reportResults(path, url, err));
+    checkFile(path, () => {
+      fs.writeFile(path, body, err => reportResults(path, url, err))
+    });
   }
 });
 
@@ -27,5 +30,20 @@ const reportResults = (filePath, url, error) => {
   }
   fs.stat(path, (err, stats) => {
     console.log(`Wrote ${stats.size} bytes to ${path}.`);
+  });
+}
+
+const checkFile = (filePath, callback) => {
+  //Check if the file exists.
+  fs.access(filePath, (err) => {
+    //If no error, that means the file exists.
+    if (!err) {
+      //Ask user if they want to overwrite
+      console.log("File already exists!");
+      callback();
+    } else {
+      //Write it
+      callback();
+    }
   });
 }
