822ae48ce048257eb3191b50bc834562066b93db
Paul Ladd <vlendle@gmail.com>
2021-03-11 14:47:44 +0000

    Added error handling for DNS, added all error reporting to reportResults

--- fetcher.js
+++ fetcher.js
@@ -4,22 +4,25 @@ const fs = require('fs');
 const url = process.argv[2];
 const path = process.argv[3];
 
-console.log(`Attempting to save ${url} contents to ${path}`);
+console.log(`Attempting to save ${url} contents to ${path}...`);
+console.log(`******************`);
 
 request(url, (error, response, body) => {
-  console.log(error);
-  console.log("*******************");
-  //console.log(response);
-  console.log("*******************");
-  fs.writeFile(path, body, err => reportResults(path, err));
+  if (error) {
+    reportResults(path, url, error);
+  } else {
+    fs.writeFile(path, body, err => reportResults(path, url, err));
+  }
 });
 
-const reportResults = (filePath, error) => {
+const reportResults = (filePath, url, error) => {
   if (error) {
-    if (error.code = "ENOENT") {
-      console.log(`ERROR: ${filePath} does not exist`);
+    if (error.code === "ENOENT") {
+      console.log(`ERROR: ${filePath} is invalid. Directory probably doesn't exist.`);
     }
-
+    if (error.code === "ENOTFOUND") {
+      console.log(`ERROR: DNS lookup failed for ${url}. URL is probably invalid.`);
+    }
     return false;
   }
   fs.stat(path, (err, stats) => {
