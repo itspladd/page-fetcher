4ab1bf396de5da4c6021540e1809e79369ebf4f0
Paul Ladd <vlendle@gmail.com>
2021-03-11 14:13:03 +0000

    Added logging of body and error to console

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,15 @@
+const request = require('request');
+const fs = require('fs');
+
 const url = process.argv[2];
 const path = process.argv[3];
 
 console.log(`Attempting to save ${url} contents to ${path}`);
+
+request(url, (error, response, body) => {
+  console.log(error);
+  console.log("*******************");
+  //console.log(response);
+  console.log("*******************");
+  console.log(body)
+});
